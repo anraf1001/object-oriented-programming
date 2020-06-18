2240e5d4401a9721f0462155c13dabe497044767
kari0036 <57415454+kari0036@users.noreply.github.com>
2020-06-18 14:22:58 +0000

    Update map.cpp

--- shm/src/map.cpp
+++ shm/src/map.cpp
@@ -1,6 +1,7 @@
 #include "map.hpp"
 
 #include <algorithm>
+
 Island* Map::getIsland(const Coordinates& coordinate) {
     return &*(std::find_if(islands_.begin(), islands_.end(),
                            [&](const auto& el) {
