b205da48881d9f36d2e3aab150c71e183cff2ed7
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-19 15:50:52 +0200

    Update shm/inc/item.hpp
    Co-authored-by: kontrybutor <r.massarnowski@gmail.com>

--- shm/inc/item.hpp
+++ shm/inc/item.hpp
@@ -18,8 +18,8 @@ public:
     //override from Cargo
     std::string getName() const override { return name_; }
     size_t getAmount() const override { return amount_; }
-    size_t getPrice() const override;
     size_t getBasePrice() const override { return basePrice_; }
+    size_t getPrice() const override;
 
 private:
     Rarity rarity_;
