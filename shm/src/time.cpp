f4093402d84f3f09a76307e98b50c1dddce6b61d
Vicx95 <wiktor.zyla31@gmail.com>
2020-06-23 16:44:34 +0200

    add Time class with functions

---
+++ shm/src/time.cpp
@@ -0,0 +1,26 @@
+#include "time.h"
+
+#include <algorithm>
+
+constexpr size_t time = 0;
+
+Time::Time()
+    : timeElapsed_(time) {
+}
+
+void Time::addObserver(Observer* obs) {
+    observers_.push_back(obs);
+}
+
+void Time::removeObserver(Observer* obs) {
+    observers_.erase(std::remove(observers_.begin(), observers_.end(), obs), vec.end());
+}
+
+size_t Time::getElapsedTime() const {
+    return timeElapsed_;
+}
+
+Time& Time::operator++() {
+    timeElapsed_++;
+    return *this;
+}
