f4e109d8fe1971057e7c10073cc0381d728d57fa
Vicx95 <wiktor.zyla31@gmail.com>
2020-06-29 18:37:51 +0200

    implement printWinScreen function

--- shm/src/game.cpp
+++ shm/src/game.cpp
@@ -1,10 +1,9 @@
 #include "game.hpp"
 
 #include "cargo.hpp"
+#include "globaltime.hpp"
 #include "island.hpp"
 #include "store.hpp"
-#include "globaltime.hpp"
-
 
 bool Game::checkWinCondition() const {
     return player_->getMoney() >= finalGoal_;
@@ -14,15 +13,26 @@ bool Game::checkLooseCondition() const {
     return player_->getMoney() == SIZE_MAX;
 }
 
-void Game::printMenu(){
+void Game::printMenu() {
     std::cout << "Your money: " << player_->getMoney()
-        << " Day: " << time_->getElapsedTime()
-        << " Days to end: " << days_ - time_->getElapsedTime()
-        << " Position: "
-        << map_->getIsland->getPosition() << "\n";
+              << " Day: " << time_->getElapsedTime()
+              << " Days to end: " << days_ - time_->getElapsedTime()
+              << " Position: "
+              << map_->getIsland->getPosition() << "\n";
 }
 
-void Game::printOptions(){
-    std::cout << "\n" << "1) Travel\n" << "2) Sell\n" << "3) Buy\n"
-        << "4) Check cargo\n" << "5) Exit\n\n" << "Choice option: ";
+void Game::printOptions() {
+    std::cout << "\n"
+              << "1) Travel\n"
+              << "2) Sell\n"
+              << "3) Buy\n"
+              << "4) Check cargo\n"
+              << "5) Exit\n\n"
+              << "Choice option: ";
+}
+
+void Game::printWinScreen(){
+    system("cls");
+    std::cout << "CONGRATULATION!!! YOU WIN, You earn: " << player_->getMoney()
+        << " money in: " << time_->getElapsedTime() << " days";
 }
