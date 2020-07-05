deee4cfaeba9c5927cbfd134e4992d1572a59875
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-05 21:12:17 +0200

    Deleted store test

--- shm/tests/storeTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "globaltime.hpp"
-#include "gtest/gtest.h"
-#include "store.hpp"
-#include "time.hpp"
-
-TEST(StoreTest, shouldCreateShopsWithoutSegFault) {
-    auto time = std::make_shared<Time>();
-    // Store store(time.get());
-}
